a68345b666c4a9bc8cad074c21467cda5c3a5432
Brock Whitten <brock@sintaxi.com>
2009-07-17 12:00:15 -0700

    now referencing submodule for helpers

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,16 +1,17 @@
 system.use("com.joyent.Sammy");
+system.use("smart-helpers.init");
 
-function render(that, temp, args){
-  if (!args){ args = {};}
-  
-  that.partial = function(part, i){
-    that.item = i || {};
-    return template(part);
-  }
-  
-  that.catch_content = template(temp);
-  return template("layouts/" + (args.layout || "application.html"));
-}
+// function render(that, temp, args){
+//   if (!args){ args = {};}
+//   
+//   that.partial = function(part, i){
+//     that.item = i || {};
+//     return template(part);
+//   }
+//   
+//   that.catch_content = template(temp);
+//   return template("layouts/" + (args.layout || "application.html"));
+// }
 
 GET("/", function(){
   return render(this, "index.html", {layout:"docs.html"});
