51eb9200e29b8c46268b68873a42d0373cdde45f
Michael Brooks <michael@michaelbrooks.ca>
2011-09-13 14:13:21 -0700

    Add a default template that all use.

--- lib/phonegap/jodoc.rb
+++ lib/phonegap/jodoc.rb
@@ -4,25 +4,30 @@ class JoDoc
   
   attr_accessor :input_directory
   attr_accessor :output_directory
-  attr_accessor :template_directory
   
   def initialize(input_directory, output_directory, options)
-    options[:lang] ||= 'en'
+    @input_directory      = input_directory
+    @output_directory     = output_directory
+    @template_directories = [ File.join TEMPLATE_PATH, 'default' ]
 
-    @input_directory    = input_directory
-    @output_directory   = output_directory
-    @template_directory = File.join TEMPLATE_PATH, options[:lang]
+    # add custom language template
+    if options[:lang]
+      @template_directories.push(File.join TEMPLATE_PATH, options[:lang])
+    end
 
     check_dependencies
   end
   
   def run
     # Copy HTML template assets
-    FileUtils.cp_r @template_directory, @output_directory
+    @template_directories.each do |template|
+      next unless File.directory? template
+      FileUtils.cp_r File.join(template, '.'), @output_directory
+    end
 
     # Run joDoc
     FileUtils.cd @input_directory do
-      `jodoc --output #{@output_directory} --title "PhoneGap API Documentation" --template #{@template_directory}/index.html ./`
+      `jodoc --output #{@output_directory} --title "PhoneGap API Documentation" --template #{@output_directory}/index.html ./`
     end
   end
   
@@ -31,7 +36,7 @@ class JoDoc
   def check_dependencies
     die 'The jodoc script is not in your path' unless command_exists?(JO_DOC_CLI)
 
-    [@input_directory, @template_directory].each do |directory|
+    [@input_directory, @template_directories[0]].each do |directory|
       die "The directory #{directory}/ should exist." unless File.directory? directory
     end
   end
