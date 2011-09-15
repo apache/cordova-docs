f14b9aa6b656befdb5d5397f16394ab36ef37bf4
Michael Brooks <michael@michaelbrooks.ca>
2011-09-15 14:44:31 -0700

    Add version and language menu.

---
+++ lib/phonegap/version_menu.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'json'
+require 'nokogiri'
+require 'fileutils'
+
+class VersionMenu
+  def initialize(options = {})
+    @versions = {}
+    docs_path = File.expand_path File.join(__FILE__, '..', '..', '..', 'docs')
+
+    # build hash of languages and versions
+    Dir.glob(File.join docs_path, '**', 'config.json').each do |file|
+      version  = File.basename(File.dirname file)
+      language = JSON.parse(IO.read(file))['language']
+
+      if language
+        @versions[language] ||= []
+        @versions[language].push version
+      else
+        puts "Warning: The key 'language' was not defined in #{file}"
+      end
+    end
+
+    @html = []
+
+    # generate HTML <select> output
+    @versions.keys.sort.each do |language|
+      @html.push "<optgroup label=\"#{language}\">"
+
+      @versions[language].sort.reverse.each do |version|
+        if version == options[:version]
+            @html.push "  <option value=\"version\" selected>#{version}</option>"
+        else
+            @html.push "  <option value=\"version\">#{version}</option>"
+        end
+      end
+
+      @html.push "</optgroup>"
+    end
+
+    @html = @html.join("\n").concat("\n")
+  end
+
+  def run(filename)
+    doc = Nokogiri::HTML(File.read(filename))
+
+    select_element = doc.css('#header small select')[0]
+    select_element.inner_html = @html
+
+    File.open(filename, 'w') { |file| file.write doc.to_html }
+
+    return doc.to_html
+  end
+end
--- lib/phonegap_docs.rb
+++ lib/phonegap_docs.rb
@@ -10,6 +10,7 @@ require 'update_keyword_index'
 require 'jodoc'
 require 'fileutils'
 require 'table_of_contents'
+require 'version_menu'
 require 'prettify'
 
 class PhoneGapDocs
@@ -17,7 +18,6 @@ class PhoneGapDocs
   
   attr_accessor :input_directory
   attr_accessor :output_directory
-  attr_accessor :working_directory
   
   def initialize(input_directory = nil, output_directory = nil)
     @input_directory   = input_directory  || default_input_directory
@@ -44,13 +44,13 @@ class PhoneGapDocs
         next if ['.', '..'].include? version_dir
         output_path = File.join @output_directory, language_dir, version_dir
         input_path  = File.join @input_directory,  language_dir, version_dir
-        options     = { :lang => language_dir }
+        options     = { :lang => language_dir, :version => version_dir }
         next unless File.directory? input_path
 
         copy_directory(input_path, @working_directory)
 
         puts " => Generating the PhoneGap Documentation for #{version_dir}-#{language_dir}..."
-        generated_path = after_jodoc(jodocify(before_jodoc(@working_directory), options))
+        generated_path = after_jodoc(jodocify(before_jodoc(@working_directory), options), options)
 
         move_directory(generated_path, output_path)
         empty_tmp_directory
@@ -79,8 +79,8 @@ class PhoneGapDocs
     output_directory
   end
   
-  def after_jodoc(input_directory)
-    klasses = [ AddTitle.new, UpdateIndex.new, UpdateKeywordIndex.new, TableOfContents.new, Prettify.new ]
+  def after_jodoc(input_directory, options)
+    klasses = [ AddTitle.new, UpdateIndex.new, UpdateKeywordIndex.new, TableOfContents.new, VersionMenu.new(options), Prettify.new ]
     
     klasses.each do |klass|
       each_file input_directory do |file|
