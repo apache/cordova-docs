53fef6b900518c3d150f72353db8be75feee7f11
Michael Brooks <michael@michaelbrooks.ca>
2012-01-26 20:54:00 -0800

    Auto-generate the navigation bar from index.html

--- lib/phonegap/navigation_menu.rb
+++ lib/phonegap/navigation_menu.rb
@@ -8,25 +8,54 @@ class NavigationMenu
   include FileHelpers
 
   def initialize(options = {})
-    filename = Dir.glob(File.join tmp_directory, '**', 'index.md.html')[0]
-    doc      = Nokogiri::HTML(File.read(filename))
-    @li_set  = Nokogiri::XML::NodeSet.new doc
+    @sections = []
 
-    doc.css('ul:first h2 a').each do |element|
-      li = Nokogiri::XML::Node.new 'li', doc
-      li.add_child element
-      @li_set.push li
+    filename = Dir.glob(File.join tmp_directory, '**', 'index.md.html')[0]
+    doc     = Nokogiri::HTML(File.read(filename))
+
+    h1_set   = doc.css('#home > h1')
+    ul_set   = doc.css('#home > ul')
+    count    = h1_set.count
+
+    count.times do |index|
+      links = []
+      ul_set[index].css('li > h2 > a').each { |a| links.push(a) }
+
+      @sections.push({
+        'title' => h1_set[index].text,
+        'links' => links
+      })
     end
   end
 
   def run(filename)
     doc = Nokogiri::HTML(File.read(filename))
 
-    ul_element = doc.css('#sidebar ul:first')[0]
-    ul_element.add_child @li_set
+    @sections.each do |section|
+      insert_title(section['title'], doc)
+      insert_links(section['links'], doc)
+    end
 
     File.open(filename, 'w') { |file| file.write doc.to_html }
 
     return doc.to_html
+  end
+
+  def insert_title(title, doc)
+    h1 = Nokogiri::XML::Node.new 'h1', doc
+    h1.content = title
+    doc.css('#sidebar').first.add_child(h1)
+  end
+
+  def insert_links(links, doc)
+    ul = Nokogiri::XML::Node.new 'ul', doc
+
+    links.each do |link|
+      li = Nokogiri::XML::Node.new 'li', doc
+      li.add_child(link)
+      ul.add_child(li)
+    end
+
+    doc.css('#sidebar').first.add_child(ul)
   end
 end
