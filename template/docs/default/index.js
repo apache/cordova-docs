49a9a129c0975fd054d8f2cfb78b2c57e60e3267
Andrey Kurdyumov <kant2002@gmail.com>
2015-07-24 18:05:22 +0600

    Prevent loading GA when working locally

--- template/docs/default/index.js
+++ template/docs/default/index.js
@@ -20,6 +20,7 @@
 //
 // Google Analytics
 //
+if (window.location.protocol !== "file:") {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -27,6 +28,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-64283057-1', 'auto');
 ga('send', 'pageview');
+}
 
 //
 // Syntax Highlighting
