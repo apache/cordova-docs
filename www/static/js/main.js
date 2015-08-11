a5b7c5acf80390ab7837a5a638388d3e8112ab41
Brad Gashler <bgashler1@users.noreply.github.com>
2015-08-11 16:26:56 -0700

    Responsive work on hero section

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -2,6 +2,8 @@ $(function(){ // resize the hero to the innerWidth of the window
 	var window_height = window.innerHeight;
 	var navbar_spacer = $('#_fixed_navbar_spacer').height();
 	var adjusted_window_height = window_height - navbar_spacer;
-	console.log('new height ' + adjusted_window_height)
-	$('#hero').height(adjusted_window_height);
+	if (adjusted_window_height > 800) { // do not resize on small devices
+		$('#hero').height(adjusted_window_height);
+		console.log('new height ' + adjusted_window_height)
+	}
  });
