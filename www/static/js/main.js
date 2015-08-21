2b08a73838ffdc500e9bcbf723e2c62fdf14677b
Dmitry Blotsky <dmitry.blotsky@gmail.com>
2015-08-21 14:43:57 -0700

    Fixing history.

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,9 +1,17 @@
-$(function(){ // resize the hero to the innerWidth of the window
-	var window_height = window.innerHeight;
-	var navbar_spacer = $('#_fixed_navbar_spacer').height();
-	var adjusted_window_height = window_height - navbar_spacer;
-	if (adjusted_window_height > 800) { // do not resize on small devices
-		$('#hero').height(adjusted_window_height);
-		console.log('new height ' + adjusted_window_height)
+$(function(){
+	
+	function setHeroHeight() {
+		console.log('IT works!');
+		// resize the hero to the innerWidth of the window
+		var window_height = window.innerHeight;
+		var navbar_spacer = $('#_fixed_navbar_spacer').height();
+		var adjusted_window_height = window_height - navbar_spacer;
+		if (adjusted_window_height > 549) { // do not resize on small devices
+			$('#hero').height(adjusted_window_height);
+			console.log('new height ' + adjusted_window_height)
+		}
 	}
+	setHeroHeight();
+	$(window).resize(function() { setHeroHeight() });
+	
  });
