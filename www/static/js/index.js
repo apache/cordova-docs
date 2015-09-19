63383ae51a08131249f1f3d93c39af9de413cf1e
riknoll <riknoll@microsoft.com>
2015-09-18 18:34:37 -0700

    Added browser api fallback for copy buttons on main page. This closes #356.

--- www/static/js/index.js
+++ www/static/js/index.js
@@ -108,10 +108,38 @@ $(document).ready(function () {
         document.getElementById("new_blog_count").innerHTML = new_blog_count;
     }
 
-    var copyButtons = document.getElementsByClassName("btn-copy");
-    for(var i = 0; i < copyButtons.length; i++) {
-        new ZeroClipboard(copyButtons[i]);
-    }
+    var client = new ZeroClipboard();
+    client.on("ready", function(e) {
+        var copyButtons = document.getElementsByClassName("btn-copy");
+        for(var i = 0; i < copyButtons.length; i++) {
+            client.clip(copyButtons[i]);
+        }
+    });
+
+    // In the case that flash is disabled, fall back to browser API
+    client.on("error", function(e) {
+        var copyButtons = document.getElementsByClassName("btn-copy");
+        for(var i = 0; i < copyButtons.length; i++) {
+            copyButtons[i].addEventListener("click", function(clickEvent) {
+                var id = clickEvent.target.getAttribute("data-clipboard-target");
+
+                var range = document.createRange();
+                range.selectNode(document.getElementById(id));
+
+                var select = window.getSelection();
+                select.removeAllRanges();
+                select.addRange(range);
+
+                try {
+                    document.execCommand("copy");
+                } catch(e) {
+                    // Silently fail for now
+                }
+
+                select.removeAllRanges();
+            });
+        }
+    });
 
     // Smooth scroll to anchor links
     $("a[href^='#']").on('click', function(e) {
