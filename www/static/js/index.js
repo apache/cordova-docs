f47aecf11782e8e8aa980976acb5c67e103d532c
Dmitry Blotsky <dmitry.blotsky@gmail.com>
2015-10-12 17:42:35 -0700

    Removing console.log calls.

--- www/static/js/index.js
+++ www/static/js/index.js
@@ -61,7 +61,6 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
-    console.log(document.cookie);
 }
 
 function getCookie(cname) {
@@ -72,7 +71,6 @@ function getCookie(cname) {
         while (c.charAt(0)==' ') c = c.substring(1);
         if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
     }
-    console.log(document.cookie);
     return "";
 }
 
