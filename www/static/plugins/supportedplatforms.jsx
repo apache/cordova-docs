df27ee22a80c09fe98e245ccc1d2425eee855cfb
Raphael von der Grün <raphinesse@gmail.com>
2018-07-18 16:28:28 +0200

    PROPOSAL Simplify supportedplatforms.jsx
    DISCLAIMER:
    I've not tested this, nor do I know how to write proper JSX ^^'

--- www/static/plugins/supportedplatforms.jsx
+++ www/static/plugins/supportedplatforms.jsx
@@ -3,35 +3,26 @@ var Preact = require('preact'),
     createClass = require('preact-compat').createClass,
     PlatformButton = require('./platformbutton.jsx');
 
-let SupportedPlatforms = createClass({
+const SupportedPlatforms = createClass({
     render: function () {
-        let keywords = this.props.keywords;
-        let sortedPlatforms = [
-            {present: false, icon: 'android', alt: 'Android'},
-            {present: false, icon: 'ios', alt: 'iOS'},
-            {present: false, icon: 'windows', alt: 'Windows'},
-            {present: false, icon: 'osx', alt: 'macOS'},
-            {present: false, icon: 'browser', alt: 'Browser'}
+        const keywords = new Set(this.props.keywords);
+        const sortedPlatforms = [
+            {icon: 'android', alt: 'Android'},
+            {icon: 'ios', alt: 'iOS'},
+            {icon: 'windows', alt: 'Windows'},
+            {icon: 'osx', alt: 'macOS'},
+            {icon: 'browser', alt: 'Browser'}
         ];
 
-        let platformsSupported = [];
-
-        keywords.forEach((keyword) => {
-            let target = sortedPlatforms.findIndex(platform => keyword.includes(platform.icon));
-            if (target > -1) sortedPlatforms[target].present = true;
-        });
-
-        sortedPlatforms.forEach((platform) => {
-            if (platform.present) {
-                // Becuase these images are taken from a sprite sheet, we have
+        const platformsSupported = sortedPlatforms
+            .filter(platform => keywords.has(platform))
+            .map(platform => (
+                // Because these images are taken from a sprite sheet, we have
                 // to use title rather than alt-text
-                platformsSupported.push(
-                    <li key={this.props.plugin + '-' + platform.alt}>
-                        <div className={platform.icon} title={platform.alt}></div>
-                    </li>
-                );
-            }
-        });
+                <li key={this.props.plugin + '-' + platform.alt}>
+                    <div className={platform.icon} title={platform.alt}></div>
+                </li>
+            ));
 
         return (
             <ul className="results-supported-platforms">
