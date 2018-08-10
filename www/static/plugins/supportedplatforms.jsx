16757ecd04b0cdb4b6d61c427ea2dc6a741f6003
エリス <erisu@users.noreply.github.com>
2018-08-10 18:30:03 +0900

    Remove deprecated platforms from plugin page (#848)
    * Remove deprecated platforms from plugin page
    - Additionally, added ASF license

--- www/static/plugins/supportedplatforms.jsx
+++ www/static/plugins/supportedplatforms.jsx
@@ -1,83 +1,53 @@
-var Preact = require('preact'),
-    h = require('preact').h,
-    createClass = require('preact-compat').createClass,
-    PlatformButton = require('./platformbutton.jsx');
+/**
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
-var SupportedPlatforms = createClass({
-    render: function() {
-        var keywords = this.props.keywords;
-        var sortedPlatforms = [
-            {present:false, icon: "android", alt:"Android"},
-            {present:false, icon: "ios", alt:"iOS"},
-            {present:false, icon: "windows", alt:"Windows"},
-            {present:false, icon: "blackberry", alt:"Blackberry"},
-            {present:false, icon: "ubuntu", alt:"Ubuntu"},
-            {present:false, icon: "firefox", alt:"FirefoxOS"},
-            {present:false, icon: "webos", alt:"webOS"},
-            {present:false, icon: "fireos", alt:"FireOS"},
-            {present:false, icon: "osx", alt:"OS X"},
-            {present:false, icon: "browser", alt:"Browser"}
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+*/
+
+const Preact = require('preact'); // eslint-disable-line
+const h = require('preact').h; // eslint-disable-line
+const createClass = require('preact-compat').createClass;
+const PlatformButton = require('./platformbutton.jsx'); // eslint-disable-line
+
+const SupportedPlatforms = createClass({
+    render: function () {
+        const sortedPlatforms = [
+            {keyword: 'cordova-android', icon: 'android', alt: 'Android'},
+            {keyword: 'cordova-ios', icon: 'ios', alt: 'iOS'},
+            {keyword: 'cordova-windows', icon: 'windows', alt: 'Windows'},
+            {keyword: 'cordova-osx', icon: 'osx', alt: 'macOS'},
+            {keyword: 'cordova-browser', icon: 'browser', alt: 'Browser'}
         ];
 
-        var platformsSupported = [];
-
-        // remove windows8 & windows dupe
-        if (keywords.indexOf('cordova-windows') > -1 && keywords.indexOf('cordova-windows8') > -1) {
-            keywords.splice(keywords.indexOf('cordova-windows8'), 1);
-        }
-        keywords.forEach(function(keyword) {
-            switch (keyword) {
-                case 'cordova-android':
-                    sortedPlatforms[0].present = true;
-                    break;
-                case 'cordova-ios':
-                    sortedPlatforms[1].present = true;
-                    break;
-                case 'cordova-windows8':
-                case 'cordova-windows':
-                    sortedPlatforms[2].present = true;
-                    break;
-                case 'cordova-blackberry10':
-                    sortedPlatforms[3].present = true;
-                    break;
-                case 'cordova-ubuntu':
-                    sortedPlatforms[4].present = true;
-                    break;
-                case 'cordova-firefoxos':
-                    sortedPlatforms[5].present = true;
-                    break;
-                case 'cordova-webos':
-                    sortedPlatforms[6].present = true;
-                    break;
-                case 'cordova-amazon-fireos':
-                    sortedPlatforms[7].present = true;
-                    break;
-                case 'cordova-osx':
-                    sortedPlatforms[8].present = true;
-                    break;
-                case 'cordova-browser':
-                    sortedPlatforms[9].present = true;
-                    break;
-            }
-        });
-
-        var that = this;
-
-        sortedPlatforms.forEach(function(platform) {
-            if(platform.present) {
-                // Becuase these images are taken from a sprite sheet, we have
+        const platformsSupported = sortedPlatforms
+            .filter(function (platform) {
+                return this.props.keywords.indexOf(platform.keyword) > -1;
+            }.bind(this))
+            .map(function (platform) {
+                // Because these images are taken from a sprite sheet, we have
                 // to use title rather than alt-text
-                platformsSupported.push(
-                    <li key={that.props.plugin + "-" + platform.alt}>
+                return (
+                    <li key={this.props.plugin + '-' + platform.alt}>
                         <div className={platform.icon} title={platform.alt}></div>
                     </li>
                 );
-            }
-        });
+            }.bind(this));
+
         return (
-            <ul className="results-supported-platforms">
-                {platformsSupported}
-            </ul>
+            <ul className="results-supported-platforms">{platformsSupported}</ul>
         );
     }
 });
