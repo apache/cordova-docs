2b0916c379ddf8909ea1aea56fef2b343e1a22de
erisu <erisu@users.noreply.github.com>
2021-09-15 14:16:07 +0000

    Deploying to asf-site from @ apache/cordova-docs@786264aa2a0d81db6429a5f4efe71e90931b03ec 🚀

--- static/js/index.js
+++ static/js/index.js
@@ -77,6 +77,7 @@ function checkNotification() {
     var dates = [];
     if (lastVisit != "") {
         
+        dates.push('Wed, 15 Sep 2021 00:00:00 +0000');
         dates.push('Mon, 06 Sep 2021 00:00:00 +0000');
         dates.push('Wed, 25 Aug 2021 00:00:00 +0000');
         dates.push('Mon, 23 Aug 2021 00:00:00 +0000');
