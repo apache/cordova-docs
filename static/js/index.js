1d9fcb1440366700a1242ef2e9e9fcc74ff4e39b
NiklasMerz <niklasmerz@linux.com>
2023-11-23 16:33:09 +0000

    Deploying to asf-site from @ apache/cordova-docs@01936e8e4371ff97b7b6f19c8bbbecf7d7931e03 🚀

--- static/js/index.js
+++ static/js/index.js
@@ -47,6 +47,7 @@ function checkNotification() {
     var dates = [];
     if (lastVisit != "") {
         
+        dates.push('Thu, 23 Nov 2023 00:00:00 +0000');
         dates.push('Thu, 21 Sep 2023 00:00:00 +0000');
         dates.push('Tue, 12 Sep 2023 00:00:00 +0000');
         dates.push('Wed, 06 Sep 2023 00:00:00 +0000');
